1881e0d7af8590cd942b1a1399d93b8dfdd74435
1834920 <106712896+1834920@users.noreply.github.com>
2022-07-31 13:22:59 -0400

    Fix typo, header position fixed, add read even more

--- scripts.js
+++ scripts.js
@@ -12,13 +12,33 @@ $('#more-about-me').on('click', event => {
 
   let moreAboutMeButton = document.getElementById('more-about-me');
   let moreAboutMePara = document.getElementById('about-me-2');
+  let evenMorePara = document.getElementById('about-me-3');
+  let evenMoreButton = document.getElementById('even-more-about-me');
+  let showLessButton = document.getElementById('less');
 
   // These statements are "hiding" all the "more" paragraphs
 
   moreAboutMePara.hidden = true;
   moreAboutMeButton.hidden = false;
-
+  evenMorePara.hidden = true;
+  evenMoreButton.hidden = false;
+  
   moreAboutMeButton.onclick = function() {
     moreAboutMePara.hidden = false;
     moreAboutMeButton.hidden = true;
   }
+
+  evenMoreButton.onclick = function () {
+    evenMorePara.hidden = false;
+    evenMoreButton.hidden = true;
+  }
+
+  showLessButton.onclick = function () {
+    moreAboutMePara.hidden = true;
+    evenMorePara.hidden = true;
+    moreAboutMeButton.hidden = false;
+    evenMoreButton.hidden = false;
+
+  }
+
+  
